714b90a9b9e2b09b500fd2dc0996f62e49623fa6
vala14 <vala14@ru.is>
2014-12-04 17:42:03 +0000

    breytingar í UI
    Búin að bæta við í user interface að það sé hægt að bæta við tölvu

--- consoleui.cpp
+++ consoleui.cpp
@@ -38,11 +38,19 @@ void ConsoleUI::waitForPrompt() {
 
 int ConsoleUI::respondToMessage() {
 
-    std::cout << MAIN_MENU << std::endl;
+    //std::cout << MAIN_MENU << std::endl;
 
-    std::string userRequest;
-    std::cin >> userRequest;
+    /*std::string userRequest;
+    std::cin >> userRequest;*/
+    int answer;
 
+    std::cout <<"Choose:\n\t1: Computer scientist.\n\t2: Computer" <<endl;
+    std::cin >> answer;
+
+    if(answer == 1){
+        std::cout << MAIN_MENU << std::endl;
+        std::string userRequest;
+        std::cin >> userRequest;
     try {
         // Handle all available commands and throw error on unknown ones
         if(userRequest.find("add") != std::string::npos) { //baeta við hvort vid viljum adda scientist eda computer
@@ -106,6 +114,74 @@ int ConsoleUI::respondToMessage() {
         std::cout << "Please try another command" << std::endl;
         waitForPrompt();
         clear();
+    }
+    }
+    if(answer == 2){ //tarf ad breyta ollu herna ur scientist i computer
+        std::cout << MAIN_MENU << std::endl;
+        std::string userRequest;
+        std::cin >> userRequest;
+    try {
+        // Handle all available commands and throw error on unknown ones
+        if(userRequest.find("add") != std::string::npos) { //baeta við hvort vid viljum adda scientist eda computer
+            clear();
+            computer additionalComputer = computer();
+            std::cout << "Enter the name of the computer: ";
+            std::cin.ignore();
+            std::getline(std::cin, additionalComputer.name);
+            clear();
+            std::cout << "Enter the year that the computer was created: ";
+            std::cin >> additionalComputer.constructionYear;
+            clear();
+            std::cout << "Enter the type of the computer: ";
+            std::cin >> additionalScientist.type;
+            clear();
+            std::cout << "Was the computer constructed (Yes/No): ";
+            std::cin >> additionalScientist.constructed;
+            clear();
+            computerService.addComputer(additionalComputer);
+        } else if(userRequest.find("search") != std::string::npos) {
+            clear();
+            std::string searchTerm = "";
+            std::cout << "Enter the search term: ";
+            std::cin.ignore();
+            std::getline(std::cin,searchTerm);
+            clear();
+            computer* searchResult = computerService.search(searchTerm);
+            if(searchResult) {
+                std::cout << "Computer found!!" << std::endl;
+                std::cout << "Name:\t\tConstruction Year:\tType:\tComputer constructed:\n";
+                std::cout << searchResult->name << "\t" << searchResult->constructionYear << "\t\t" << searchResult->type << "\t\t" << searchResult->constructed << "\t\t" << std::endl;
+            } else {
+                std::cout << "No results found for the term: " << searchTerm << std::endl;
+            }
+            waitForPrompt();
+            clear();
+        } else if(userRequest.find("order") != std::string::npos) {
+            clear();
+            std::string filterCol = "";
+            std::string filterMod = "";
+            std::cout << FILTER_MENU << std::endl;
+            std::cin >> filterCol >> filterMod;
+            clear();
+            std::list<computer> l = computerService.getComputersOrderedBy(filterCol,filterMod);
+            std::cout << "Name:\t\tYear of construction:\tType:\tComputer constructed:\n";
+            for(std::list<computer>::iterator iter = l.begin(); iter != l.end(); iter ++) {
+                std::cout << iter->name << "\t" << iter->constructionYear << "\t\t" << iter->type << "\t\t" << iter->constructed << std::endl;
+            }
+            waitForPrompt();
+            clear();
+        } else if (userRequest.find("exit") != std::string::npos) {
+            return 0;
+        } else {
+            throw std::runtime_error( userRequest + " is not a valid command.");
+        }
+    } catch(std::runtime_error e) {
+        clear();
+        std::cout << "Command caused an error: " << e.what() << std::endl;
+        std::cout << "Please try another command" << std::endl;
+        waitForPrompt();
+        clear();
+    }
     }
 
     return 1;
